7a9134bd8741cdadc8d5947e9b9a51d544abea05
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-29 23:19:39 +0300

    deploy

--- partials/header.php
+++ partials/header.php
@@ -10,25 +10,27 @@
     </div>
   </div>
 
-  <div class="header-second">
-    <div class="ui-container ui-container_large header-second__container">
-      <a href="/" class="header-logo">
-        <img src="<?php bloginfo('template_url') ?>/dist/images/logo.png" alt="<?php bloginfo('name') ?>" class="header-logo__image" />
-        <span class="header-logo__desc">ремонт    строительство    аренда</span>
-      </a>
+  <div class="header-second-wrapper">
+    <div class="header-second">
+      <div class="ui-container ui-container_large header-second__container">
+        <a href="/" class="header-logo">
+          <img src="<?php bloginfo('template_url') ?>/dist/images/logo.png" alt="<?php bloginfo('name') ?>" class="header-logo__image" />
+          <span class="header-logo__desc">ремонт    строительство    аренда</span>
+        </a>
 
-      <?php wp_nav_menu([
-        'container' => false,
-        'theme_location' => 'menu-main',
-        'menu_class' => 'header-menu'
-      ]) ?>
+        <?php wp_nav_menu([
+          'container' => false,
+          'theme_location' => 'menu-main',
+          'menu_class' => 'header-menu'
+        ]) ?>
 
-      <div class="header-contacts">
-        <div class="header-contacts__group">
-          <a href="tel:<?php the_field('theme_phone', 'options') ?>" class="header-contacts__phone"><?php the_field('theme_phone', 'options') ?></a>
-          <a href="mailto:<?php the_field('theme_email', 'options') ?>" class="header-contacts__email"><?php the_field('theme_email', 'options') ?></a>
+        <div class="header-contacts">
+          <div class="header-contacts__group">
+            <a href="tel:<?php the_field('theme_phone', 'options') ?>" class="header-contacts__phone"><?php the_field('theme_phone', 'options') ?></a>
+            <a href="mailto:<?php the_field('theme_email', 'options') ?>" class="header-contacts__email"><?php the_field('theme_email', 'options') ?></a>
+          </div>
+          <button class="header-contacts__callback"></button>
         </div>
-        <button class="header-contacts__callback"></button>
       </div>
     </div>
   </div>
