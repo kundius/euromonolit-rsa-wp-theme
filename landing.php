bd4d1dd7cef6544b129e9e70ff196a2ede80fd4c
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-30 14:08:58 +0300

    deploy

--- landing.php
+++ landing.php
@@ -28,7 +28,84 @@ Template Name: Главная
               </div>
             </div>
             <div class="intro-layout__form">
-              form
+              <form action="/wp-json/contact-form-7/v1/contact-forms/19/feedback" method="post" class="intro-form js-form">
+                <div class="intro-form__title">
+                  Заказать<br />
+                  обратный звонок
+                </div>
+
+                <div class="intro-form__row">
+                  <input type="text" name="your-name" class="ui-input" placeholder="Имя:" />
+                </div>
+
+                <div class="intro-form__row">
+                  <span class="wpcf7-form-control-wrap your-phone">
+                    <input type="tel" name="your-phone" value="" class="ui-input" placeholder="Телефон*">
+                  </span>
+                </div>
+
+                <div class="intro-form__row">
+                  <textarea rows="4" name="your-message" class="ui-textarea" placeholder="Дополнительная информация:"></textarea>
+                </div>
+
+                <div class="intro-form__rules">
+                  <span class="wpcf7-form-acceptance-wrap">
+                    <label class="ui-rules">
+                      <input type="checkbox" name="rules" value="1" class="form-checkbox">
+                      <span></span>
+                      Прочитал(-а) <a href="<?php the_permalink(49) ?>" target="_blank">Пользовательское соглашение</a> и&nbsp;соглашаюсь с&nbsp;<a href="<?php the_permalink(3) ?>" target="_blank">Политикой конфиденциальности</a>
+                    </label>
+                  </span>
+                </div>
+
+                <div class="intro-form__submit">
+                  <button type="submit" class="ui-button-submit ui-button-submit_glare">
+                    <span class="ui-loader-square intro-form__loader"></span>
+                    Отправить
+                  </button>
+                </div>
+
+                <div class="intro-form__success">
+                  <div class="intro-form-result intro-form-result_success">
+                    <div class="intro-form-result__head">
+                      <div class="intro-form-result__head-icon"></div>
+                      <div class="intro-form-result__head-title">
+                        Ваше сообщение
+                        успешно отправлено
+                      </div>
+                    </div>
+                    <div class="intro-form-result__body">
+                      <div class="intro-form-result__body-text">
+                        В ближайшее время<br />
+                        мы свяжемся с вами.
+                      </div>
+                      <div class="intro-form-result__body-close wpcf7-form-status-reset">
+                        Закрыть окно
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="intro-form__failed">
+                  <div class="intro-form-result intro-form-result_failed">
+                    <div class="intro-form-result__head">
+                      <div class="intro-form-result__head-icon"></div>
+                      <div class="intro-form-result__head-title">
+                        Возникла ошибка
+                      </div>
+                    </div>
+                    <div class="intro-form-result__body">
+                      <div class="intro-form-result__body-text">
+                        Не удалось<br />
+                        отправить сообщение
+                      </div>
+                      <div class="intro-form-result__body-close wpcf7-form-status-reset">
+                        Закрыть окно
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
