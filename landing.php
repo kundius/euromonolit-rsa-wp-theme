aae8ba78323148a0bd1ba96f5a50f7dc72fd77ba
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-05-01 00:20:36 +0300

    deploy

--- landing.php
+++ landing.php
@@ -160,7 +160,9 @@ $products = new WP_Query([
             <div class="landing-catalog__products-item">
               <article class="catalog-card">
                 <figure class="catalog-card__image">
-                  <img src="<?php the_post_thumbnail_url('theme-medium') ?>" alt="<?php the_title() ?>" />
+                  <div class="catalog-card__image-wrap">
+                    <img src="<?php the_post_thumbnail_url('theme-medium') ?>" alt="<?php the_title() ?>" />
+                  </div>
                 </figure>
                 <div class="catalog-card__body">
                   <h2 class="catalog-card__title">
