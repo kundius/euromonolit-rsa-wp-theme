515864c82e214366eceb8d38a473f2303af9ecdf
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-30 18:47:23 +0300

    deploy

--- landing.php
+++ landing.php
@@ -2,6 +2,12 @@
 /*
 Template Name: Главная
 */
+$products = new WP_Query([
+  'post_type' => 'page',
+  'post_parent' => 13,
+  'order' => 'ASC',
+  'orderby' => 'menu_order',
+]);
 ?>
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes() ?> itemscope itemtype="http://schema.org/WebSite">
@@ -112,6 +118,57 @@ Template Name: Главная
               </form>
             </div>
           </div>
+        </div>
+      </section>
+
+      <?php $landing_catalog = get_field('landing_catalog') ?>
+      <section class="landing-catalog">
+        <div class="ui-container">
+
+          <?php if (!empty($landing_catalog['title'])): ?>
+          <div class="landing-catalog__title">
+            <?php echo $landing_catalog['title'] ?>
+          </div>
+          <?php endif ?>
+
+          <?php if (!empty($landing_catalog['subtitle'])): ?>
+          <div class="landing-catalog__subtitle">
+            <?php echo $landing_catalog['subtitle'] ?>
+          </div>
+          <?php endif ?>
+
+          <?php if (!empty($landing_catalog['content'])): ?>
+          <div class="landing-catalog__content">
+            <?php echo $landing_catalog['content'] ?>
+          </div>
+          <?php endif ?>
+
+          <?php if ($products->have_posts()): ?>
+          <div class="landing-catalog__products">
+            <?php while ($products->have_posts()): ?>
+            <?php $products->the_post()?>
+            <div class="landing-catalog__products-item">
+              <article class="catalog-card">
+                <figure class="catalog-card__image">
+                  <img src="<?php the_post_thumbnail_url('theme-medium') ?>" alt="<?php the_title() ?>" />
+                </figure>
+                <div class="catalog-card__body">
+                  <h2 class="catalog-card__title">
+                    <?php the_title() ?>
+                  </h2>
+                  <div class="catalog-card__more">
+                    <a href="<?php the_permalink() ?>" class="ui-button-more">
+                      Узнать больше
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <?php endwhile?>
+            <?php wp_reset_postdata()?>
+          </div>
+          <?php endif?>
+
         </div>
       </section>
 
