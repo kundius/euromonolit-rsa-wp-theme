2f7128b24b43707860c70b6d69e722852bd3428b
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-05-05 15:16:57 +0300

    deploy

--- archive.php
+++ archive.php
@@ -10,55 +10,53 @@
       <?php get_template_part('partials/header') ?>
       <?php get_template_part('partials/page-breadcrumbs') ?>
 
-      <div class="archive-section">
+      <div class="page-headline">
         <div class="ui-container">
-          <h1 class="archive-section__title"><?php single_cat_title() ?></h1>
-          <div class="archive-layout">
-            <div class="archive-layout__content">
-              <?php if (have_posts()): ?>
-              <div class="archive-grid">
-                <?php while (have_posts()): ?>
-                <?php the_post()?>
-                <div class="archive-grid__cell">
-                  <article class="archive-card">
-                    <figure class="archive-card__image">
-                      <img src="<?php the_post_thumbnail_url('theme-medium')?>" alt="<?php the_title()?>" />
-                    </figure>
-                      <div class="archive-card__body">
-                        <div class="archive-card__date">
-                          <?php the_date('d.m.Y')?>
-                        </div>
-                        <h2 class="archive-card__title">
-                          <a href="<?php the_permalink()?>"><?php the_title()?></a>
-                        </h2>
-                        <div class="archive-card__excerpt">
-                          <?php the_excerpt()?>
-                        </div>
-                      </div>
-                      <div class="archive-card__tags">
-                        <?php the_tags('')?>
-                      </div>
-                  </article>
-                </div>
-                <?php endwhile?>
-                <?php wp_reset_postdata()?>
-              </div>
-
-              <?php the_posts_pagination(['prev_text' => '', 'next_text' => ''])?>
-              <?php else: ?>
-                <p>Извините, ничего не найдено.</p>
-              <?php endif?>
-            </div>
-            
-            <div class="archive-layout__side">
-              <div class="archive-layout__side-sicky">
-                <?php get_template_part('partials/side-news')?>
-                <?php get_template_part('partials/side-subscribe')?>
-              </div>
-            </div>
-          </div>
+          <h1 class="page-headline__title"><?php single_cat_title() ?></h1>
         </div>
       </div>
+
+      <section class="page-archive">
+        <div class="ui-container">
+          <?php if (have_posts()): ?>
+          <div class="archive-grid js-magic-grid">
+            <?php while (have_posts()): ?>
+            <?php the_post() ?>
+            <div class="archive-grid__cell">
+              <article class="archive-card">
+                <figure class="archive-card__image">
+                  <img src="<?php the_post_thumbnail_url('theme-medium') ?>" alt="<?php the_title() ?>" />
+                </figure>
+                <div class="archive-card__body">
+                  <div class="archive-card__date">
+                    <?php the_date('d.m.Y') ?>
+                  </div>
+                  <h2 class="archive-card__title">
+                    <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                  </h2>
+                  <div class="archive-card__excerpt">
+                    <?php the_excerpt() ?>
+                  </div>
+                </div>
+                <div class="archive-card__tags">
+                  <?php the_tags('') ?>
+                </div>
+              </article>
+            </div>
+            <?php endwhile?>
+            <?php wp_reset_postdata()?>
+          </div>
+
+          <?php the_posts_pagination(['prev_text' => '', 'next_text' => ''])?>
+          <?php else: ?>
+            <p>Извините, ничего не найдено.</p>
+          <?php endif?>
+
+          <div class="page-archive__content ui-content">
+            <?php term_description() ?>
+          </div>
+        </div>
+      </section>
 
       <?php get_template_part('partials/footer')?>
     </div>
--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -2,6 +2,7 @@
 // import "lazysizes/plugins/native-loading/ls.native-loading";
 // import "lazysizes/plugins/object-fit/ls.object-fit";
 // import "what-input";
+import MagicGrid from "magic-grid"
 
 import "./modal";
 import "./modalAttachment";
@@ -20,3 +21,18 @@ import "./onScroll";
 // lazySizes.cfg.nativeLoading = {
 //   setLoadingAttribute: false,
 // };
+
+let magicGrid = new MagicGrid({
+  container: ".js-magic-grid", // Required. Can be a class, id, or an HTMLElement
+  // static: false, // Required for static content. Default: false.
+  // items: 30, // Required for dynamic content. Initial number of items in the container.
+  gutter: 15, // Optional. Space between items. Default: 25(px).
+  // maxColumns: 5, // Optional. Maximum number of columns. Default: Infinite.
+  useMin: true, // Optional. Prioritize shorter columns when positioning items? Default: false.
+  // useTransform: true, // Optional. Position items using CSS transform? Default: True.
+  // animate: true, // Optional. Animate item positioning? Default: false.
+  // center: true, //Optional. Center the grid items? Default: true. 
+});
+
+magicGrid.listen();
+
