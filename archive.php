a879be870ec3ae87b8e2f9c5a3aae2918dfa819a
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-05-06 02:44:48 +0300

    u

--- archive.php
+++ archive.php
@@ -26,7 +26,7 @@ $query_params = [
   //   'post__in' => ''
   // ],
   'orderby' => [
-    'is_sticky' => 'ASC',
+    // 'is_sticky' => 'ASC',
     'date' => 'DESC',
   ],
   // 'meta_key' => 'is_sticky',
