385ac807172b956568a021b628a4397b5c46c8b1
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-05-06 03:03:29 +0300

    u

--- inc/Services/Post.php
+++ inc/Services/Post.php
@@ -25,10 +25,11 @@ class Post implements Service
      */
     public function boot(Service_Container $container): void
     {
-      \add_action('save_post', [$this, 'on_save_post'], 10, 3);
+      \add_action('save_post', [$this, 'on_change_post']);
+      \add_action('edit_post', [$this, 'on_change_post']);
     }
 
-    public function on_save_post($post_id, $post, $update) {
+    public function on_change_post($post_id) {
       $sticky = \get_option('sticky_posts');
       $is_sticky = in_array($post_id, $sticky);
       if (!\update_post_meta($post_id, 'is_sticky', $is_sticky)) {
