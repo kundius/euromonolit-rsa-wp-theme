2e3f811f6389074a548f50234b0eb1b126a71bae
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-05-11 01:06:48 +0300

    deploy

--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -22,17 +22,19 @@ import "./onScroll";
 //   setLoadingAttribute: false,
 // };
 
-let magicGrid = new MagicGrid({
-  container: ".js-magic-grid", // Required. Can be a class, id, or an HTMLElement
-  static: true, // Required for static content. Default: false.
-  // items: 30, // Required for dynamic content. Initial number of items in the container.
-  gutter: 0, // Optional. Space between items. Default: 25(px).
-  // maxColumns: 5, // Optional. Maximum number of columns. Default: Infinite.
-  useMin: true, // Optional. Prioritize shorter columns when positioning items? Default: false.
-  // useTransform: true, // Optional. Position items using CSS transform? Default: True.
-  // animate: true, // Optional. Animate item positioning? Default: false.
-  // center: true, //Optional. Center the grid items? Default: true. 
-});
+if (document.querySelector(".js-magic-grid")) {
+  let magicGrid = new MagicGrid({
+    container: ".js-magic-grid", // Required. Can be a class, id, or an HTMLElement
+    static: true, // Required for static content. Default: false.
+    // items: 30, // Required for dynamic content. Initial number of items in the container.
+    gutter: 0, // Optional. Space between items. Default: 25(px).
+    // maxColumns: 5, // Optional. Maximum number of columns. Default: Infinite.
+    useMin: true, // Optional. Prioritize shorter columns when positioning items? Default: false.
+    // useTransform: true, // Optional. Position items using CSS transform? Default: True.
+    // animate: true, // Optional. Animate item positioning? Default: false.
+    // center: true, //Optional. Center the grid items? Default: true. 
+  });
 
-magicGrid.listen();
+  magicGrid.listen();
 
+}
